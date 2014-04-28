21643aef8ec67769e7bcaff36278f54bdfb623d3
Mike Drabic <mdrabic@skookum.com>
2014-04-28 10:15:00 -0400

    Update links in header when user is signed in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirecto_to user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
